f8025828005c866a65bae413d279ea48c4eac761
Ubuntu <ubuntu@ip-172-31-0-181.us-east-2.compute.internal>
2025-06-07 13:14:35 +0000

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='quante_carlo',
+      version='0.1',
+      description='The most efficent implementation of Monte Carlo in the world.',
+      url='https://quantecarlo.com',
+      author='sign-of-fourier',
+      author_email='info@quantecarlo.com',
+      license='MIT',
+      packages=['promptimizer'],
+      zip_safe=False)
