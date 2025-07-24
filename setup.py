b59fb6154717a01251ef0702997705be9a1a38d7
Ubuntu <ubuntu@ip-172-31-0-181.us-east-2.compute.internal>
2025-07-24 15:26:35 +0000

    openai

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='promptimizer',
-      version='0.5.5',
+      version='0.5.6',
       description='Prompt Optimization server.',
       url='https://quantecarlo.com',
       author='sign-of-fourier',
