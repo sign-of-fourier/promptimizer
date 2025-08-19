ccb196f37bac86e8f4dea2f796c0022ca684bfec
Ubuntu <ubuntu@ip-172-31-0-181.us-east-2.compute.internal>
2025-08-19 01:26:32 +0000

    ops

---
+++ promptimizer/user_db.py
@@ -0,0 +1,156 @@
+import boto3
+from datetime import datetime as dt
+import random
+import string
+# Initialize a session using Amazon DynamoDB
+import pandas as pd
+
+bucket = 'sagemaker-us-east-2-344400919253'
+
+initial_df = pd.DataFrame({'prompt_id': [0], 
+                           'use_case': ['messy'],
+                           'writer_system': ['My First Saved Prompt'],
+                           'writer_user': ['that too'],
+                           'task_system': ['task_system'],
+                           'separator': ['---'],
+                           'label': ['probability'],
+                           'evaluator': ['AUC'],
+                           'setup_id': ['jbn5'], # can be used to find training data and embeddings if available
+                           'save_date': [dt.now()]
+                           })
+
+
+class dynamo_client:
+    def __init__(self):
+        dynamodb = boto3.resource('dynamodb')
+
+        self.db = dynamodb.Table('Users')
+      #  self.x_map = {'customer_id': ':cust',
+      #                'firstname': ':fname',
+      #                'lastname': ':lname',
+      #                'n_credits': ':cred',
+      #                'password': ':pass'}
+
+
+    def new_user(self, P):
+
+        exists = self.get_user(P)
+
+        if exists:
+            return 'Customoer Already Exists'
+        else:
+
+            item = {'transaction_time': str(dt.now())}
+            for p in ['email_address', 'firstname', 'lastname', 'n_credits',
+                      'password']:
+                item[p] = P[p]
+
+            response = self.db.put_item(Item=item)
+            if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+
+                initial_df.to_csv('s3://' + bucket+'/users/{}/saved_prompts.csv'.format(P['email_address']), index=False)
+
+                return 'Success'
+            else:
+                return 'HTTP Failure'
+
+    def everything(self):
+
+        items = []
+        response = self.db.scan()
+        items.extend(response.get('Items', []))
+
+        while 'LastEvaluatedKey' in response:
+            response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
+            items.extend(response.get('Items', []))
+
+        print("All items in the table:")
+        for item in items:
+            print(item)
+
+
+    def get_user(self, P):
+        
+        if P['email_address'] == '':
+            return None
+        else:
+            response = self.db.get_item(Key={'email_address': P['email_address']})
+            return response.get('Item')
+
+
+    def delete_user(self, P):
+        return self.db.delete_item(Key={'email_address': P['email_address'] })
+
+    def decrement_usage(self):
+        c = self.get_customer(P['email_address'])
+        print(c)
+        n_credits = c['n_credits']
+        print(n_credits, n_credits-P['delta'])
+        self.update_customer(email_address, {'n_credits': n_credits-P['delta']})
+
+
+    def increment_usage(self, P):
+
+        u = self.get_user(P['email_address'])
+
+        if n_credits + P['delta'] < 0:
+            response = self.db.update_item(
+                    Key={'customer_id': P['email_address']},  
+                    UpdateExpression="SET n_credits = 0",
+                    ReturnValues="UPDATED_NEW"
+                    )
+
+            return 'This will drop user to less than zero credits'
+            print (response['Attributes'])
+        else:
+            response = self.db.update_item(
+                    Key={'customer_id': P['email_address']},  # Specify the primary key
+                    UpdateExpression="SET n_credits = n_credits + :inc",
+                    ExpressionAttributeValues = {':inc': P['delta']},
+                    ReturnValues="UPDATED_NEW"
+                    )
+            print("Update succeeded:", response['Attributes'])
+            return 'Success'
+
+
+ 
+
+
+    def update_customer(self, customer_id, updates):
+
+        update_string = []
+        Xpression = {}
+
+        for u in updates.keys():
+            update_string.append(u + ' = ' + self.x_map[u])
+            Xpression[self.x_map[u]] = updates[u]
+
+        print(','.join(update_string))
+        response = self.db.update_item(
+                Key={'customer_id': customer_id},  # Specify the primary key
+                #UpdateExpression="SET subscription_type = :stype, n_credits = n_credits + :inc",
+                UpdateExpression='SET + update_string',
+                ExpressionAttributeValues=Xpression,
+                #{
+                #    ':stype': 'Potato',
+                #    ':inc': 13
+                #    },
+                ReturnValues="UPDATED_NEW"
+                )
+        print("Update succeeded:", response['Attributes'])
+
+#if __name__ == '__main__':
+#    db = customer_db()
+#    new_customer = db.new_customer('Liza',  'Cher', 'Plastic', 100, 'imma star')
+    #print(new_customer)
+    #print(db.get_customer(new_customer))
+    #db.delete_customer('OYFWGZYO')
+    #db.increment_usage('UEmlWPbX', -10)
+    #db.everything()
+#    db.update_customer('CUST12345', {'credits': 10})
+#    print(db.get_customer('CUST12345'))
+
+
+
+
+
