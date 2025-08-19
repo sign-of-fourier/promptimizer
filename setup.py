188d44daa33c9913c3dc995bfdbd2c5db8abd1b8
Ubuntu <ubuntu@ip-172-31-0-181.us-east-2.compute.internal>
2025-08-19 01:27:37 +0000

    ops2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='promptimizer',
-      version='0.6.9',
+      version='0.7',
       description='Prompt Optimization server.',
       url='https://quantecarlo.com',
       author='sign-of-fourier',
